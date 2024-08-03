96d69e375b0a3390a5b94184658edd9a18296ee8
arul9976 <arunmicheal8@gmail.com>
2024-08-03 13:54:51 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ghPages()
+  ],
+
+  base: '/ChatWithTodo/',
+  server: {
+    host: '0.0.0.0', // Or any desired host/IP
+  },
 })
